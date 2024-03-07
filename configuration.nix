f15c43c4d0624412ba2cec6a4abdcd44244ac042
rw <rw@jjk.is>
2024-03-07 09:06:45 -0800

    make sure user exists

--- configuration.nix
+++ configuration.nix
@@ -5,9 +5,7 @@
 
   # Users configuration
   users.users.robertwendt = {
-    isNormalUser = true;
     home = "/Users/robertwendt";
     createHome = true; # Ensures the home directory is created
-    shell = pkgs.zsh; # Sets the default shell for the user
   };
 }
