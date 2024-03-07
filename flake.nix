ed162ed1f237c4e00d6fc8dde153dcf62f0caaa1
r33drichards <rwendt1337@gmail.com>
2024-03-07 11:15:06 -0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
 
   outputs = inputs@{ nixpkgs, home-manager, darwin, ... }: {
     darwinConfigurations = {
-      Roberts-MacBook-Air = darwin.lib.darwinSystem {
+      rws-MacBook-Air = darwin.lib.darwinSystem {
         system = "aarch64-darwin";
         modules = [
           ./configuration.nix
