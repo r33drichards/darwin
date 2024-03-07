a9d26bf9a302f7a9df348cd35db73064b55678ca
r33drichards <rwendt1337@gmail.com>
2024-03-07 11:13:13 -0800

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -10,4 +10,22 @@
   };
   services.nix-daemon.enable = true;
 
+  environment.systemPackages = [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.vscode
+    pkgs.git
+    pkgs.mosh
+    pkgs.emacs
+    # ssh
+    pkgs.openssh
+    pkgs.nix-direnv
+    pkgs.thefuck
+    pkgs.poetry
+    # pip
+    pkgs.python3Packages.pip
+    pkgs.nixpkgs-fmt
+    pkgs.tailscale
+  ];
+
 }
