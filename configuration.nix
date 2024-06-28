f61f054ba5a34b1027a1899e887c05148a8fcf9b
rw <rw@jjk.is>
2024-06-27 19:02:42 -0700

    not used

--- configuration.nix
+++ configuration.nix
@@ -71,44 +71,6 @@
   # services.tailscale.enable = true;
   security.pam.enableSudoTouchIdAuth = true;
 
-  # todo tailscale up --ssh 
-  # https://tailscale.com/kb/1215/oauth-clients#registering-new-nodes-using-oauth-credentials
-
-  # launchd.daemons.tailscale = {
-  #   serviceConfig = {
-  #     RunAtLoad = true;
-  #     StandardOutPath = "/tmp/tailscale.out.log";
-  #     StandardErrorPath = "/tmp/tailscale.err.log";
-  #   };
-  #   script = with  pkgs;''
-  #     echo "tailscale up"
-
-  #     source ${config.system.build.setEnvironment}
-  #     sleep 2
-
-  #     # check if we are already authenticated to tailscale
-  #     status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
-  #     if [ $status = "Running" ]; then # if so, then do nothing
-  #       echo "tailscale is already running"
-  #       exit 0
-  #     fi
-  #     ${pkgs.tailscale}/bin/tailscale up --ssh
-  #   '';
-  # };
-
-  # launchd.agents.tailscaled = {
-  #   serviceConfig = {
-  #     RunAtLoad = true;
-  #     KeepAlive = true;
-  #     StandardOutPath = "/tmp/tailscaled.out.log";
-  #     StandardErrorPath = "/tmp/tailscaled.err.log";
-  #   };
-  #   script = with  pkgs; ''
-  #     ${tailscale}/bin/tailscaled
-  #   '';
-  # };
-
-  # I'd rather not have telemetry on my package manager.
   environment.variables.HOMEBREW_NO_ANALYTICS = "1";
 
   homebrew = {
@@ -124,67 +86,5 @@
       Tailscale = 1475387142; # App Store URL id
     };
   };
-
-
-  launchd.agents.pf3000 = {
-    serviceConfig = {
-      RunAtLoad = true;
-      KeepAlive = true;
-      StandardOutPath = "/tmp/pf3000.out.log";
-      StandardErrorPath = "/tmp/pf3000.err.log";
-    };
-    script = with  pkgs; ''
-      ssh -N -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -L 3000:localhost:3000 alice@devenv
-    '';
-  };
-
-  launchd.agents.pf8000 = {
-    serviceConfig = {
-      RunAtLoad = true;
-      KeepAlive = true;
-      StandardOutPath = "/tmp/pf8000.out.log";
-      StandardErrorPath = "/tmp/pf8000.err.log";
-    };
-    script = with  pkgs;''
-      ssh -N -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -L 8000:localhost:8000 alice@devenv
-    '';
-  };
-
-  launchd.agents.pf8080 = {
-    serviceConfig = {
-      RunAtLoad = true;
-      KeepAlive = true;
-      StandardOutPath = "/tmp/pf8080.out.log";
-      StandardErrorPath = "/tmp/pf8080.err.log";
-    };
-    script = with  pkgs;''
-      ssh -N -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -L 8080:localhost:8080 alice@devenv
-    '';
-  };
-
-  launchd.agents.pf8081 = {
-    serviceConfig = {
-      RunAtLoad = true;
-      KeepAlive = true;
-      StandardOutPath = "/tmp/pf8081.out.log";
-      StandardErrorPath = "/tmp/pf8081.err.log";
-    };
-    script = with  pkgs;''
-      ssh -N -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -L 8081:localhost:8081 alice@devenv
-    '';
-  };
-  launchd.agents.pf4000 = {
-    serviceConfig = {
-      RunAtLoad = true;
-      KeepAlive = true;
-      StandardOutPath = "/tmp/pf4000.out.log";
-      StandardErrorPath = "/tmp/pf4000.err.log";
-    };
-    script = with  pkgs;''
-      ssh -N -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -L 4000:localhost:4000 alice@devenv
-    '';
-  };
-
-
 
 }
