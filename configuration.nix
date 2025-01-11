b587c6e7edbb1850aa7cf4eac0871f125d554356
Your Name <you@example.com>
2025-01-11 12:57:23 -0800

    vc

--- configuration.nix
+++ configuration.nix
@@ -7,7 +7,21 @@ in
 
 {
   # Run the linux-builder as a background service
-  nix.linux-builder.enable = true;
+
+  nix.linux-builder = {
+    enable = true;
+    ephemeral = true;
+    maxJobs = 4;
+    config = {
+      virtualisation = {
+        darwin-builder = {
+          diskSize = 40 * 1024;
+          memorySize = 8 * 1024;
+        };
+        cores = 6;
+      };
+    };
+  };
 
   nix.settings.substituters = [
     "https://nix-community.cachix.org"
